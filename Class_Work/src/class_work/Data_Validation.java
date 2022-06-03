a08db3c630132044c4f4030a72859aafd26929e1
Matthew Oosthuyse <moosthuyse1999@gmail.com>
2022-06-03 21:38:51 +0200

    Testing adding files from netbeans

---
+++ Class_Work/src/class_work/Data_Validation.java
@@ -0,0 +1,17 @@
+
+package class_work;
+
+/**
+ *
+ * @author mat
+ */
+public class Data_Validation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
